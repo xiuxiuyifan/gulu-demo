35244154481622f0c8a79d4ce78faf06a601a3cb
xiuxiuyifan <1575243732@qq.com>
2018-12-12 22:58:58 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,23 +1,32 @@
 <template>
-    <button class="g-button">hi</button>
+    <button class="g-button">
+        <svg class="i" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <slot></slot>
+    </button>
 </template>
 <script>
-    export default{}
+    export default {
+        props:{
+            icon:String
+        }
+    }
 </script>
 <style lang="scss" scoped>
-.g-button{
-    height: var(--button-height);
-    background-color: var(--button-bg);
-    font-size: var(--font-size);
-    border-radius: var(--border-radius);
-    border-color: var(--border-color);
-    margin: 200px;
-    padding: 0 1em;
-    &:hover{
-        background-color: var(--button-active-bg);
+    .g-button {
+        height: var(--button-height);
+        background-color: var(--button-bg);
+        font-size: var(--font-size);
+        border-radius: var(--border-radius);
+        margin: 200px;
+        padding: 0 1em;
+        border: 1px solid var(--border-color);
+        &:hover {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
     }
-    &:focus{
-        outline: none;
-    }
-}
 </style>
