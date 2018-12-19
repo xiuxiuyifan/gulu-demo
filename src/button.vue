e2d76ebecd8b84c9f5ca15a8e1a68d530bb95db2
xiuxiuyifan <1575243732@qq.com>
2018-12-19 23:35:37 +0800

    给lodding添加动效

--- src/button.vue
+++ src/button.vue
@@ -1,32 +1,67 @@
 <template>
-    <button class="g-button">
-        <svg class="i" aria-hidden="true">
-            <use :xlink:href=`#i-${icon}`></use>
-        </svg>
-        <slot></slot>
+    <button class="g-button" :class="`i-${position} `" v-if="icon" @click="clickAfter">
+        <g-icon :icon="icon"></g-icon>
+        <span class="g-word"><slot></slot></span>
     </button>
 </template>
 <script>
-    export default {
-        props:{
-            icon:String
+import Icon from "./icon";
+export default {
+  name: "g-button",
+  props: {
+    icon: {
+      type: String
+    },
+    position: {
+      type: String,
+      default: "left", //校验只能是left或者right
+      validator: function(value) {
+        if (value === "left" || value === "right") {
+          return true;
+        } else {
+          console.error("position属性只接受left或者right");
+          return false;
         }
+      }
     }
+  },
+  component: {
+    "g-icon": Icon
+  },
+  methods: {
+    clickAfter() {
+        this.emit()
+      console.log("我被点击了");
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-    .g-button {
-        height: var(--button-height);
-        background-color: var(--button-bg);
-        font-size: var(--font-size);
-        border-radius: var(--border-radius);
-        margin: 200px;
-        padding: 0 1em;
-        border: 1px solid var(--border-color);
-        &:hover {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
-    }
+.i-right {
+  order: 1;
+}
+.g-button {
+  display: inline-flex;
+  align-items: center;
+  vertical-align: middle;
+  line-height: var(--button-height);
+  background-color: var(--button-bg);
+  font-size: var(--font-size);
+  border-radius: var(--border-radius);
+  padding: 0 1em;
+  border: 1px solid var(--border-color);
+  box-sizing: border-box;
+  & .g-word {
+    font-size: 14px;
+  }
+  &:hover {
+    background-color: var(--button-active-bg);
+    border: 1px solid #409eff;
+    cursor: pointer;
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid #409eff;
+  }
+}
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,37 @@
+<template>
+    <svg aria-hidden="true" class="g-icon" :class="{rotate:false}">
+        <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+</template>
+
+<script>
+export default {
+  name: "icon",
+  props: {
+    icon: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.g-icon {
+  width: 1em;
+  height: 1em;
+  margin-right: 0.5em;
+  display: inline-block;
+  vertical-align: middle;
+}
+.rotate{
+    animation: lodding 1s linear infinite;
+}
+@keyframes lodding {
+    0%{
+        transform: rotate(0);
+    }
+    100%{
+        transform:rotate(360deg);
+    }
+}
+</style>
