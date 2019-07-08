726c5a2916ca0bf34a67b6926c47615fd23197a9
xiuxiuyifan <1575243732@qq.com>
2019-07-08 22:38:15 +0800

    使用vue 特性的无缝轮播基本功能完成

--- src/g-carousel-item.vue
+++ src/g-carousel-item.vue
@@ -1,82 +1,111 @@
 <template>
-  <transition name="carousel" mode="out-in">
-    <div class="g-carousel-item" :class="{'z-index': zIndex}" v-show="selected === currentKey">
+  <transition :name="carousel" mode="in-out">
+    <div :class="{'z-index': zIndex}" class="g-carousel-item" v-show="selected === currentKey">
       <slot></slot>
     </div>
   </transition>
 </template>
 
 <script>
-export default {
-  name: "g-carousel-item",
-  inject: ["eventBus"],
-  data() {
-    return {
-      selected: 0,
-      currentKey: 0,
-      zIndex: false //控制选中的z-index层级
-    };
-  },
-  mounted() {
-    this.currentKey = this.$vnode.key;
-    console.log(this.selected === this.currentKey);
-    this.eventBus.$on("update:selected", data => {
-      console.log("当前选中的" + data);
-      //根据传递过来的data 可以判断是向左还是向右
-      // console.log("原来是" + this.currentKey);
-      //如果是当前选中的再看左右，忽略其他改变状态的通知
-      // if (data === this.currentKey) {
-      //   if (data > this.currentKey) {
-      //     console.log("向右");
-      //   } else {
-      //     console.log("向左");
-      //   }
-      // }
-      if (data === this.currentKey) {
-        this.zIndex = true;
-      } else {
-        this.zIndex = false;
+  export default {
+    name: 'g-carousel-item',
+    inject: ['eventBus'],
+    data () {
+      return {
+        selected: 0,
+        currentKey: 0,
+        zIndex: false,          //控制选中的z-index层级
+        lastIndex: 0,           //上一次选中的
+        carousel: undefined,    //动画的方向
       }
-      this.selected = data;
-    });
+    },
+    mounted () {
+      this.currentKey = this.$vnode.key
+      console.log(this.selected === this.currentKey)
+      this.eventBus.$on('update:selected', (data) => {
+        console.log(data)
+        const { selected, direction } = data
+        console.log('当前选中的' + selected)
+        //根据传递过来的data 可以判断是向左还是向右
+        if (direction === 'next') {
+          this.carousel = 'carousel-next'
+        } else {
+          this.carousel = 'carousel-previous'
+        }
+        this.zIndex = selected === this.currentKey
+        this.selected = selected
+      })
+    },
   }
-};
 </script>
 
 <style lang="scss" scoped>
-.carousel-enter {
-  transform: translateX(100%);
-}
+  .carousel-next-enter {
+    transform: translateX(100%);
+  }
 
-.carousel-enter-active {
-  transition: all 0.4s ease-in-out;
-}
+  .carousel-next-enter-active {
+    transition: all 0.4s ease-in-out;
+  }
 
-.carousel-enter-to {
-  transform: translateX(0%);
-}
-.carousel-leave {
-  transform: translateX(0%);
-}
-.carousel-leave-active {
-  transition: all 0.4s ease-in-out;
-}
-.carousel-leave-to {
-  transform: translateX(-100%);
-}
-.g-carousel-item {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  text-align: center;
-  &-wrapper {
+  .carousel-next-enter-to {
+    transform: translateX(0%);
+  }
+
+  .carousel-next-leave {
+    transform: translateX(0%);
+  }
+
+  .carousel-next-leave-active {
+    transition: all 0.4s ease-in-out;
+  }
+
+  .carousel-next-leave-to {
+    transform: translateX(-100%);
+  }
+
+  /*
+    previous
+  */
+  .carousel-previous-enter {
+    transform: translateX(-100%);
+  }
+
+  .carousel-previous-enter-active {
+    transition: all .4s ease-in-out;
+  }
+
+  .carousel-previous-enter-to {
+    transform: translateX(0%);
+  }
+
+  .carousel-previous-leave {
+    transform: translateX(0%);
+  }
+
+  .carousel-previous-leave-active {
+    transition: all .4s ease-in-out;
+  }
+
+  .carousel-previous-leave-to {
+    transform: translateX(100%);
+  }
+
+  .g-carousel-item {
     width: 100%;
     height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    text-align: center;
+
+    &-wrapper {
+      width: 100%;
+      height: 100%;
+    }
   }
-}
-.z-index {
-  z-index: 1;
-}
+
+  .z-index {
+    z-index: 1;
+  }
 </style>
--- src/g-carousel.vue
+++ src/g-carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="g-carousel" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+  <div @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" class="g-carousel">
     <slot></slot>
     <!--按钮-->
     <div @click="change('previous')" class="switch-button previous">
@@ -10,165 +10,198 @@
     </div>
     <!--指标-->
     <div class="g-carousel-flag">
-      <div class="item" v-for="(item,index) in  childrens" :key="index">
-        <div></div>
+      <div :key="index" @click="clickTrigger(index)" class="item" v-for="(item,index) in  childrens">
+        <div :class="{'current-bg':index === selected}"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import GIcon from "./icon";
-import Vue from "vue";
+  import GIcon from './icon'
+  import Vue from 'vue'
 
-export default {
-  name: "g-carousel",
-  components: {
-    GIcon
-  },
-  props: {
-    selected: {
-      type: Number,
-      default: 0
+  export default {
+    name: 'g-carousel',
+    components: {
+      GIcon,
     },
-    autoPlay: {
-      type: Boolean,
-      default: true
-    }
-  },
-  data() {
-    return {
-      childrens: 0,
-      eventBus: new Vue(),
-      timerId: undefined
-    };
-  },
-  provide() {
-    return {
-      eventBus: this.eventBus
-    };
-  },
-  mounted() {
-    //拿到当前传进来的 items 的个数生成下面的
-    this.childrens = this.$slots.default.length;
-    //通知子组件当前要选中哪一个
-    this.eventBus.$emit("update:selected", this.selected);
-    if (this.autoPlay) {
-      this.playAuto();
-    }
-  },
-  methods: {
-    change(type) {
-      //prevoius 减
-      //next 加
-      const action = {
-        previous: this.selected - 1,
-        next: this.selected + 1
-      };
-      console.log("前一个" + action.previous);
-      if (action.previous === -1) {
-        action.previous = this.childrens - 1;
-      }
-      if (action.next === this.childrens) {
-        action.next = 0;
-      }
-      console.log("结果" + action.previous);
-      this.$emit("update:selected", action[type]);
-      this.eventBus.$emit("update:selected", action[type]);
+    props: {
+      selected: {
+        type: Number,
+        default: 0,
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true,
+      },
     },
-    playAuto() {
-      let fn = () => {
+    data () {
+      return {
+        childrens: 0,
+        eventBus: new Vue(),
+        timerId: undefined,
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus,
+      }
+    },
+    mounted () {
+      //拿到当前传进来的 items 的个数生成下面的
+      this.childrens = this.$slots.default.length
+      //通知子组件当前要选中哪一个
+      this.eventBus.$emit('update:selected', { selected: this.selected, direction: 'next' })
+      if (this.autoPlay) {
+        this.playAuto()
+      }
+      console.log(this.selected)
+    },
+    'methods': {
+      'change' (type) {
+        //prevoius 减
+        //next 加
         const action = {
-          next: this.selected + 1
-        };
+          previous: this.selected - 1,
+          next: this.selected + 1,
+        }
+        if (action.previous === -1) {
+          action.previous = this.childrens - 1
+        }
         if (action.next === this.childrens) {
-          action.next = 0;
+          action.next = 0
         }
-        this.$emit("update:selected", action.next);
-        this.eventBus.$emit("update:selected", action.next);
-        if (this.autoPlay) {
-          this.timerId = setTimeout(fn, 3000);
+        this.$emit('update:selected', action[type])
+        this.eventBus.$emit('update:selected', { selected: action.next, direction: type })
+      },
+      playAuto () {
+        let fn = () => {
+          const action = {
+            next: this.selected + 1,
+          }
+          if (action.next === this.childrens) {
+            action.next = 0
+          }
+          this.$emit('update:selected', action.next)
+          this.eventBus.$emit('update:selected', { selected: action.next, direction: 'next' })
+          if (this.autoPlay) {
+            this.timerId = setTimeout(fn, 3000)
+          }
         }
-      };
-      this.timerId = setTimeout(fn, 3000);
+        this.timerId = setTimeout(fn, 3000)
+      },
+      clickTrigger (index) {
+        if (index > this.selected) {
+          this.eventBus.$emit('update:selected', { selected: index, direction: 'next' })
+        } else {
+          this.eventBus.$emit('update:selected', { selected: index, direction: 'previous' })
+        }
+        this.$emit('update:selected', index)
+      },
+      pause () {
+        window.clearTimeout(this.timerId)
+        this.timerId = undefined
+      },
+      onMouseEnter () {
+        this.pause()
+      },
+      onMouseLeave () {
+        this.playAuto()
+      },
     },
-    pause() {
-      window.clearTimeout(this.timerId);
-      this.timerId = undefined;
-    },
-    onMouseEnter() {
-      this.pause();
-    },
-    onMouseLeave() {
-      this.playAuto();
-    }
   }
-};
 </script>
 
 <style lang="scss" scoped>
-.g-carousel {
-  position: relative;
-  overflow: hidden;
-  width: 590px;
-  height: 470px;
-  margin: 0 auto;
-  > div.g-carousel-item:nth-child(even) {
-    background: #d3dce6;
-  }
-  > div.g-carousel-item:nth-child(odd) {
-    background: #99a9bf;
-  }
-  .switch-button {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    background: rgba(31, 45, 61, 0.11);
-    border-radius: 50%;
-    width: 36px;
-    height: 36px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    z-index: 10;
-  }
+  .g-carousel {
+    position: relative;
+    overflow: hidden;
+    width: 590px;
+    height: 470px;
+    margin: 0 auto;
 
-  .previous {
-    left: 10px;
-  }
+    &:hover {
+      .previous {
+        opacity: 1;
+        transform: translateX(15px) translateY(-50%);
+      }
 
-  .next {
-    right: 10px;
-  }
+      .next {
+        opacity: 1;
+        transform: translateX(-15px) translateY(-50%);
+      }
+    }
 
-  .g-carousel-flag {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    transform: translateX(-50%);
-    bottom: 0;
-    text-align: center;
+    > div.g-carousel-item:nth-child(even) {
+      background: #d3dce6;
+    }
 
-    > .item {
-      display: inline-block;
-      padding: 12px 4px;
+    > div.g-carousel-item:nth-child(odd) {
+      background: #99a9bf;
+    }
 
-      > div {
-        background: rebeccapurple;
-        height: 100%;
-        display: block;
-        opacity: 0.48;
-        width: 30px;
-        height: 2px;
-        border: none;
-        outline: none;
-        padding: 0;
-        margin: 0;
-        cursor: pointer;
-        transition: 0.3s;
+    .switch-button {
+      display: flex;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      background: rgba(31, 45, 61, 0.11);
+      border-radius: 50%;
+      width: 36px;
+      height: 36px;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      z-index: 10;
+      transition: all .5s;
+      opacity: 0;
+
+      &:hover {
+        background: rgba(31, 45, 61, .23);
+      }
+    }
+
+    .previous {
+      left: 10px;
+    }
+
+    .next {
+      right: 10px;
+    }
+
+    .g-carousel-flag {
+      position: absolute;
+      z-index: 10;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 0;
+      text-align: center;
+      cursor: pointer;
+
+      > .item {
+        display: inline-block;
+        padding: 12px 4px;
+
+        .current-bg {
+          background: #00adb5;
+        }
+
+        > div {
+          height: 100%;
+          background: #eeeeee;
+          display: block;
+          opacity: 0.48;
+          width: 30px;
+          height: 2px;
+          border: none;
+          outline: none;
+          padding: 0;
+          margin: 0;
+          cursor: pointer;
+          transition: 0.3s;
+        }
       }
     }
   }
-}
 </style>
